3618c18031e44c1998a229446d62fdb1b68bb513
DillFennel <88512253+DillFennel@users.noreply.github.com>
2025-05-04 15:45:09 +0300

    Update mathTree.c

--- mathTree.c
+++ mathTree.c
@@ -1,63 +1,56 @@
 #include <stdio.h>
 
-#define wrongOperationError printf("Error: wrong operation\n") / 
+#define wrongOperationError printf("Error: wrong operation\n"); \
             exit(1);
-#define checkOperation(op) if (op != '-' && op != '+' /
-        && op != '*' && op != '/' /
-        && op != '0'){ /
-            wrongOperationError/
-        }
 
-#define checkChildAns(child) if (!child.ans) { /
-            printf("Error: child's ans is empty\n") /
-            exit(1); /
+#define checkChildAns(child) if (!child->ans) { \
+            printf("Error: child's ans is empty\n"); \
+            exit(1); \
         }
-struct MathTree{
-    MathTree(char _operation){ // Empty struct
-        checkOperation( _operation)
-        left = nullptr;
-        right = nullptr;
-        ans = NULL;
-        operation = _operation;
+        
+enum operations { plus, minus, multiply, divide, none };
+
+typedef struct MathTree{
+    struct MathTree *left;
+    struct MathTree *right;
+    double ans;
+    enum operations oper;
+} MathTree;
+
+
+struct MathTree* makeVariale(double val){ // Single variable
+    struct MathTree* ret = malloc(sizeof(MathTree));
+    ret->ans = val;
+    ret->oper = operations(none);
+}
+struct MathTree* makeTree(enum operations _operation, MathTree* _left, MathTree* _right){
+    checkChildAns(_left)
+    checkChildAns(_right)
+    struct MathTree* ret = malloc(sizeof(MathTree));
+    ret->left = _left;
+    ret->right = _right;
+    ret->oper = _operation;
+    switch (_operation){
+        case minus:
+            ret->ans = _left->ans - _right->ans;
+            break;
+        case plus:
+            ret->ans = _left->ans + _right->ans;
+            break;
+        case multiply:
+            ret->ans = _left->ans * _right->ans;
+            break;
+        case divide:
+            ret->ans = _left->ans / _right->ans;
+            break;
+        default:
+            wrongOperationError
     }
-    MathTree(double val){ // Single variable
-        left = nullptr;
-        right = nullptr;
-        ans = val;
-        operation = '0';
-    }
-    MathTree(char _operation, Tree* _left, Tree* _right){
-        checkChildAns(_left)
-        checkChildAns(_right)
-        left = _left;
-        right = _right;
-        switch (operation){
-            case '-':
-                ans = left.ans - right.ans;
-                break;
-            case '+':
-                ans = left.ans + right.ans;
-                break;
-            case '*':
-                ans = left.ans  right.ans;
-                break;
-            case '/':
-                ans = left.ans / right.ans;
-                break;
-            default:
-                wrongOperationError
-        }
-    }
-    void addLeftChild
-    void addRightChild
-    
-    MathTree *left;
-    MathTree *right;
-    doble ans;
-    char operation; // - + * / 0
 }
 int main() {
-	// your code goes here
+	struct MathTree *a = makeVariale(3);
+	struct MathTree *b = makeVariale(2);
+	struct MathTree *c = makeTree(operations(plus), a, b);
 
 }
 
